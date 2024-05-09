c302c5015ecee5302c3f8717c8a4c77f481add39
Aatir Manzur <aatrapps@gmail.com>
2024-05-08 20:35:15 -0400

    add msgqueue

---
+++ msgqueue/msgqueue.h
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TRUE  1
+#define FALSE	0
+
+enum Msg_T{
+	ledMsg,
+	divChkMsg
+};
+
+enum MsgOrigin_T{
+	task1,
+	task2,
+	task3
+};
+
+enum MsgDest_T{
+	task1_dest,
+	task2_dest,
+	task3_dest
+};
+
+typedef struct Msg{
+	enum Msg_T msg_t;
+	enum MsgOrigin_T  msgorigin_t;
+	enum MsgDest_T msgdest_t;
+} MSG_T;
+
+typedef struct Node_t {
+    MSG_T data;
+    struct Node_t *prev;
+} NODE;
+
+/* the HEAD of the Queue, hold the amount of node's that are in the queue*/
+typedef struct Queue {
+    NODE *head;
+    NODE *tail;
+    int size;
+    int limit;
+} Queue;
+
+Queue *ConstructQueue(int limit);
+void DestructQueue(Queue *queue);
+int Enqueue(Queue *pQueue, NODE *item);
+NODE *Dequeue(Queue *pQueue);
+int isEmpty(Queue* pQueue);
--- product/product.c
+++ product/product.c
@@ -8,11 +8,31 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "../blink/include/blink.h"
+#include "../msgqueue/msgqueue.h"
 
 void main()
 {
     stdio_init_all();
     printf("Product test\n");
+
+    int i;
+    Queue *pQ = ConstructQueue(7);
+    NODE *pN;
+
+    for (i = 0; i < 7; i++) {
+        pN = (NODE*) malloc(sizeof (NODE));
+        pN->data.msgdest_t = task2;
+        pN->data.msgorigin_t = task1;
+        pN->data.msg_t = ledMsg;
+        Enqueue(pQ, pN);
+    }
+
+    while (!isEmpty(pQ)) {
+        pN = Dequeue(pQ);
+        printf("\nDequeued: %d", pN->data);
+        free(pN);
+    }
+    DestructQueue(pQ);
 
     struct led_task_arg arg1 = { 15, 100 };
     xTaskCreate(led_task, "LED_Task 1", 256, &arg1, 1, NULL);
